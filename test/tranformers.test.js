decfab5512a56712e3449030ba8afcc7a6a59d28
Steele Walston <steele.walston@gmail.com>
2018-04-04 17:42:17 -0700

    color tansforming tests work and added third test

--- test/tranformers.test.js
+++ test/tranformers.test.js
@@ -1,6 +1,7 @@
 const assert = require('assert');
 const invert = require('../lib/invert-transformer');
 const grayscale = require('../lib/grayscale-transformer');
+const grayscale2 = require('../lib/grayscale-transformer2');
 
 describe('transformers', () => {
 
@@ -17,7 +18,7 @@ describe('transformers', () => {
             g: 155,
             b: 50
         });
-    });
+    }),
 
     it('grayscale', () => {
         // HINT: grayscale assigns the average of all three colors
@@ -33,7 +34,21 @@ describe('transformers', () => {
             g: 113,
             b: 113
         });
-    });
-
+    }),
     // TODO: add a third transformer
+    it('grayscale2', () => {
+        // HINT: grayscale assigns the average of all three colors
+        // as the new value for each color
+        const transformed = grayscale2({
+            r: 34,
+            g: 100,
+            b: 205
+        });
+
+        assert.deepEqual(transformed, {
+            r: 119.5,
+            g: 119.5,
+            b: 119.5
+        });
+    });
 });
