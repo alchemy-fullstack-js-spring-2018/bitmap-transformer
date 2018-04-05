c19909cda77868bdeea335f3d8e21af7cc31ef10
Stephanie Smith <stephanie.lauren.design@gmail.com>
2018-04-04 17:45:33 -0700

    started working on test

--- test/bitmap-transformer.test.js
+++ test/bitmap-transformer.test.js
@@ -7,11 +7,12 @@ describe('bitmap file transformer', () => {
     
     let buffer = null;
     beforeEach(() => {
-        // TODO: read './test/test-bitmap.bmp' into buffer variable
+        // TODOne: read './test/test-bitmap.bmp' into buffer variable
         // Okay to use `sync` file methods for now
+        buffer = fs.readFileSync('./test/test-bitmap.bmp');
 
         // TODO: If the functionality in this before test is same as 
-        // other test, can you remove (extract) the duplication?
+        // other test, can you remove (extract) the duplication? 
     });
 
     // "pinning" test, or "snapshot" test
