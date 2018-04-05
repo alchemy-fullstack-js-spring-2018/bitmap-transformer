c9aa4560fcd22f3e419fbf82e16e920683eaa7a5
Stephanie Smith <stephanie.lauren.design@gmail.com>
2018-04-05 11:25:53 -0700

    removed comments

--- test/bitmap-header.test.js
+++ test/bitmap-header.test.js
@@ -7,13 +7,10 @@ describe('bitmap header', () => {
 
     let buffer = null;
     beforeEach(() => {
-        // TODOne: read './test/test-bitmap.bmp' into buffer variable
-        // Go ahead and use the "sync" version of the fs methods
         buffer = fs.readFileSync('./test/test-bitmap.bmp');
     });
 
     it('has correct specs', () => {
-        // TODOne: read the wiki spec docs to figure out what these values should be
         const newConstant = new Constants();
 
         assert.ok(newConstant.PIXEL_OFFSET);
@@ -22,14 +19,13 @@ describe('bitmap header', () => {
     });
 
     it('parses header data', () => {
-        // TODOne: use the constants to populate the following properties
-        // on the BitmapHeader in its constructor.
-        // These test values are correct for the supplied test-bitmap.bmp
         const header = new BitmapHeader(buffer);
         const newConstant = new Constants();
+
         header.pixelOffset = buffer.readIntLE(newConstant.PIXEL_OFFSET, 4);
         header.bitsPerPixel = buffer.readIntLE(newConstant.BITS_PER_PIXEL_OFFSET, 2);
         header.fileSize = buffer.readIntLE(newConstant.FILE_SIZE_OFFSET, 4);
+        
         assert.equal(header.pixelOffset, 54);
         assert.equal(header.bitsPerPixel, 24);
         assert.equal(header.fileSize, 30054);
