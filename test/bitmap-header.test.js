210a9b58dc5d38ee5585d9be368d7817bb2cec86
Stephanie Smith <stephanie.lauren.design@gmail.com>
2018-04-04 17:00:29 -0700

    wrote tests to check constans and offsets

--- test/bitmap-header.test.js
+++ test/bitmap-header.test.js
@@ -1,27 +1,35 @@
 const assert = require('assert');
-const constants = require('../lib/bitmap-constants');
+const Constants = require('../lib/bitmap-constants');
 const BitmapHeader = require('../lib/bitmap-header');
+const fs = require('fs');
 
 describe('bitmap header', () => {
 
     let buffer = null;
     beforeEach(() => {
-        // TODO: read './test/test-bitmap.bmp' into buffer variable
+        // TODOne: read './test/test-bitmap.bmp' into buffer variable
         // Go ahead and use the "sync" version of the fs methods
+        buffer = fs.readFileSync('./test/test-bitmap.bmp');
     });
 
     it('has correct specs', () => {
-        // TODO: read the wiki spec docs to figure out what these values should be
-        assert.ok(constants.PIXEL_OFFSET);
-        assert.ok(constants.BITS_PER_PIXEL_OFFSET);
-        assert.ok(constants.FILE_SIZE_OFFSET);
+        // TODOne: read the wiki spec docs to figure out what these values should be
+        const newConstant = new Constants();
+
+        assert.ok(newConstant.PIXEL_OFFSET);
+        assert.ok(newConstant.BITS_PER_PIXEL_OFFSET);
+        assert.ok(newConstant.FILE_SIZE_OFFSET);
     });
 
-    it('parses header data', () => {
-        // TODO: use the constants to populate the following properties
+    it.only('parses header data', () => {
+        // TODOne: use the constants to populate the following properties
         // on the BitmapHeader in its constructor.
         // These test values are correct for the supplied test-bitmap.bmp
         const header = new BitmapHeader(buffer);
+        const newConstant = new Constants();
+        header.pixelOffset = buffer.readIntLE(newConstant.PIXEL_OFFSET, 4);
+        header.bitsPerPixel = buffer.readIntLE(newConstant.BITS_PER_PIXEL_OFFSET, 2);
+        header.fileSize = buffer.readIntLE(newConstant.FILE_SIZE_OFFSET, 4);
         assert.equal(header.pixelOffset, 54);
         assert.equal(header.bitsPerPixel, 24);
         assert.equal(header.fileSize, 30054);
