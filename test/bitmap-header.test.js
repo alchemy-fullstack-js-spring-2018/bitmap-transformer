77b03097acb3670dbec4d9b440d755a23b8325d3
J3ffcon1 <Jrlone1990@gmail.com>
2018-04-05 10:37:34 -0700

    fix indent problems

--- test/bitmap-header.test.js
+++ test/bitmap-header.test.js
@@ -19,19 +19,21 @@ describe('bitmap header', () => {
         // TODONE: read the wiki spec docs to figure out what these values should be
         
     
-        assert.ok(constants.PIXEL_OFFSET, 10); //10 bits
-        assert.ok(constants.BITS_PER_PIXEL_OFFSET, 28);//32 bits
-        assert.ok(constants.FILE_SIZE_OFFSET, 2); //32 bits
+        assert.ok(constants.PIXEL_OFFSET); 
+        assert.ok(constants.BITS_PER_PIXEL_OFFSET);
+        assert.ok(constants.FILE_SIZE_OFFSET); 
     });
 
-});
 
-it('parses header data', () => {
+
+    it('parses header data', () => {
     // TODO: use the constants to populate the following properties
     // on the BitmapHeader in its constructor.
     // These test values are correct for the supplied test-bitmap.bmp
-    const header = new BitmapHeader(buffer);
-    assert.equal(header.pixelOffset, 54);
-    assert.equal(header.bitsPerPixel, 24);
-    assert.equal(header.fileSize, 30054);
+        const header = new BitmapHeader(buffer);
+        assert.equal(header.pixelOffset, 54);
+        assert.equal(header.bitsPerPixel, 24);
+        assert.equal(header.fileSize, 30054);
+    });
+
 });
