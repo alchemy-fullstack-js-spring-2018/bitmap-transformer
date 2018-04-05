a012bdd405e33b9d9c352ddcc13d10578ec0e510
hnrzzle <henrique.j.oliveira1@gmail.com>
2018-04-05 12:16:33 -0700

    remove comment

--- test/bitmap-transformer.test.js
+++ test/bitmap-transformer.test.js
@@ -10,9 +10,6 @@ describe('bitmap file transformer', () => {
     let buffer = null;
     beforeEach(() => {
         buffer = fs.readFileSync('./test/test-bitmap.bmp');
-
-        // TODO: If the functionality in this before test is same as 
-        // other test, can you remove (extract) the duplication?
     });
 
     it('test whole transform', () => {
