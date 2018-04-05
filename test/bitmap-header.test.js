14202562c0171d380a13dff04a66d229a5e46485
Jen Lipton <jkl.lipton@gmail.com>
2018-04-05 09:45:37 -0700

    working transform

--- test/bitmap-header.test.js
+++ test/bitmap-header.test.js
@@ -1,29 +1,23 @@
 const assert = require('assert');
 const constants = require('../lib/bitmap-constants');
 const BitmapHeader = require('../lib/bitmap-header');
-const fs = require('fs');
 
 describe('bitmap header', () => {
 
     let buffer = null;
     beforeEach(() => {
-        // TODOne: read './test/test-bitmap.bmp' into buffer variable
-        // Go ahead and use the "sync" version of the fs methods
 
-        buffer = fs.readFileSync('./test/test-bitmap.bmp');
+        buffer = constants.buffer;
+
     });
 
     it('has correct specs', () => {
-        // TODOne: read the wiki spec docs to figure out what these values should be
         assert.ok(constants.PIXEL_OFFSET);
         assert.ok(constants.BITS_PER_PIXEL_OFFSET);
         assert.ok(constants.FILE_SIZE_OFFSET);
     });
 
     it('parses header data', () => {
-        // TODOne: use the constants to populate the following properties
-        // on the BitmapHeader in its constructor.
-        // These test values are correct for the supplied test-bitmap.bmp
         const header = new BitmapHeader(buffer);
         assert.equal(header.pixelOffset, 54);
         assert.equal(header.bitsPerPixel, 24);
