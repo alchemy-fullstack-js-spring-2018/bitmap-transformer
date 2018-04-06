6f262dcdf651958b7e5b364bb9c5556ca0635f0a
hnrzzle <henrique.j.oliveira1@gmail.com>
2018-04-06 10:48:18 -0700

    start rewriting bitmap-header

--- test/bitmap-header.test.js
+++ test/bitmap-header.test.js
@@ -1,6 +1,4 @@
 const assert = require('assert');
-const fs = require('fs');
-const constants = require('../lib/bitmap-constants');
 const BitmapHeader = require('../lib/bitmap-header');
 
 describe('bitmap header', () => {
@@ -8,19 +6,8 @@ describe('bitmap header', () => {
     // TODO: Update test for new bitmap-header code
     // Use mocha async test
 
-    let buffer = null;
-    beforeEach(() => {
-        buffer = fs.readFileSync('./test/test-bitmap.bmp');
-    });
-
-    it('has correct specs', () => {
-        assert.ok(constants.PIXEL_OFFSET);
-        assert.ok(constants.BITS_PER_PIXEL_OFFSET);
-        assert.ok(constants.FILE_SIZE_OFFSET);
-    });
-
-    it('parses header data', () => {
-        const header = new BitmapHeader(buffer);
+    it.only('parses header data', () => {
+        const header = new BitmapHeader('./test/test-bitmap.bmp');
         assert.equal(header.pixelOffset, 54);
         assert.equal(header.bitsPerPixel, 24);
         assert.equal(header.fileSize, 30054);
