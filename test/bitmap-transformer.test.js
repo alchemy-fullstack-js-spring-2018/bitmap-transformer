6ddfe4633e8436c6cc20c9f450d915d55b6ad9e4
Steele Walston <steele.walston@gmail.com>
2018-04-09 11:46:26 -0700

    transform test works with chunking and writeStream

--- test/bitmap-transformer.test.js
+++ test/bitmap-transformer.test.js
@@ -4,24 +4,16 @@ const BitmapTransformer = require('../lib/bitmap-transformer');
 const invert = require('../lib/invert-transformer');
 
 describe('bitmap file transformer', () => {
-    const actualFile = './test/test-bitmap.bmp';
-    // let buffer = null;
-    
-    // beforeEach(() => {
-    //     buffer = fs.readFileSync('./test/test-bitmap.bmp');
-    // });
-
-    // it('create header', () => {
-    //     const bitmap = new BitmapTransformer(buffer);
-    //     bitmap.create(buffer);
-    // });
-
+    const actualFile = './test/actual-invert-bitmap.bmp';
     it('test whole transform', () => {
         return BitmapTransformer.create('./test/test-bitmap.bmp')
             .then ((header) => {
-                header.transform(invert, actualFile);
-                const expected = fs.readFileSync('./test/inverted-expected.bmp');
-                assert.deepEqual(header, expected);
+                return header.transform(invert, actualFile)
+                    .then(() => {
+                        const actual = fs.readFileSync(actualFile);
+                        const expected = fs.readFileSync('./test/inverted-expected.bmp');
+                        assert.deepEqual(actual, expected);
+                    });
             });
 
 
