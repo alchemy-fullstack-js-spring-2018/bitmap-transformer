0d50323cf2c249f7799188c7d04316da158c5899
Katlyn Tucker <studioquirk@yahoo.com>
2018-04-12 10:52:07 -0700

    changed path to inverted,ceated new file, added image missing

--- test/bitmap-transformer.test.js
+++ test/bitmap-transformer.test.js
@@ -1,31 +1,27 @@
 const assert = require('assert');
 const fs = require('fs');
 const bitmapTransformer = require('../lib/bitmap-transformer');
-const invert = require('../lib/invert-transformer');
+const invert = require('../lib/inverted-transformer');
 const { promisify } = require('util');
 const unlink = promisify(require('fs').unlink);
 
 describe('bitmap file transformer', () => {
-    
-
-  
     let tester = null;
     const file = './test/test-bitmap.bmp';
-    const testFile = './test/inverted-expected.bmp';
+    //const testFile = './test/inverted-expected.bmp';
 
     beforeEach(() => {
-        return unlink(testFile)
+        return unlink(file)
             .catch(err => {
                 if(err.code !== 'ENOENT') throw err;
             });
     });
+
     beforeEach(() => {
         return bitmapTransformer.create(file)
             .then(data => {
-
                 tester = data;
                 return tester;
-
             });
     });
 
@@ -33,9 +29,13 @@ describe('bitmap file transformer', () => {
     it('test whole transform', () => {
         //let buffer = buffer;
         const bitmap = new bitmapTransformer(tester);
-        bitmap.transform(invert);
+        return tester.transform(invert, './test/invert-transformer-test.bmp')
+            .then( () => {
+                const expected = fs.readFileSync('./test/inverted-expected.bmp');
+                const tester = fs.readFileSync('./')
+                assert.deepEqual(bitmap.tester, expected);
 
-        const expected = fs.readFileSync('./inverted-expected.bmp');
-        assert.deepEqual(bitmap.tester, expected);
+            }
+
     });
 });
