8c7813fe5ac11cdc47a85ec0f57ec44a5c64f596
Katlyn Tucker <studioquirk@yahoo.com>
2018-04-12 18:47:13 -0700

    bitmap image keeps disappearing from files

--- test/bitmap-transformer.test.js
+++ test/bitmap-transformer.test.js
@@ -1,22 +1,22 @@
 const assert = require('assert');
 const fs = require('fs');
 const bitmapTransformer = require('../lib/bitmap-transformer');
-const invert = require('../lib/inverted-transformer');
-const { promisify } = require('util');
-const unlink = promisify(require('fs').unlink);
+const invert = require('../lib/invert-transformer');
+//const { promisify } = require('util');
+//const picture = promisify(require('fs').unlink);
+//const testFile =  require('./test/inverted-expected.bmp');
 
 describe('bitmap file transformer', () => {
     let tester = null;
     const file = './test/test-bitmap.bmp';
-    //const testFile = './test/inverted-expected.bmp';
 
     beforeEach(() => {
-        return unlink(file)
+        return (file)
             .catch(err => {
                 if(err.code !== 'ENOENT') throw err;
             });
     });
-
+        
     beforeEach(() => {
         return bitmapTransformer.create(file)
             .then(data => {
@@ -24,18 +24,16 @@ describe('bitmap file transformer', () => {
                 return tester;
             });
     });
-
+        
     //"snapshot" test
     it('test whole transform', () => {
         //let buffer = buffer;
         const bitmap = new bitmapTransformer(tester);
-        return tester.transform(invert, './test/invert-transformer-test.bmp')
-            .then( () => {
-                const expected = fs.readFileSync('./test/inverted-expected.bmp');
-                const tester = fs.readFileSync('./')
-                assert.deepEqual(bitmap.tester, expected);
-
-            }
-
+        return tester.transform(invert, './inverted-expected.bmp')
+            .then(() => {
+                //const testFile = fs.readFileSync('./test/inverted-expected.bmp');
+                const tester = fs.readFileSync('./');
+                assert.deepEqual(bitmap.tester, tester);
+            });
     });
 });
